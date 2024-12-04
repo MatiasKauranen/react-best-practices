d2391d7b8ee3afb91b8d61c24f2e4517880ff794
Veikka Kursula <kursulaveikka@gmail.com>
2024-12-04 13:46:40 +0200

    Localstorage support and better folder structure (#4)

---
+++ src/components/Ui/index.ts
@@ -0,0 +1,6 @@
+export { default as EditIcon } from "./Icons/EditIcon";
+export { default as TrashIcon } from "./Icons/TrashIcon";
+export { default as CheckIcon } from "./Icons/CheckIcon";
+export { default as CheckboxIcon } from "./Icons/CheckboxIcon";
+export { default as PlusIcon } from "./Icons/PlusIcon";
+export { default as iconStyle } from "./Icons/iconStyle";
