b5dbeea7eafe2c0ce650463babfb8764849d1d2d
Veikka Kursula <kursulaveikka@gmail.com>
2025-01-10 15:39:48 +0200

    Dividing logic into more manageable parts (#6)

---
+++ src/components/Ui/taskContainer.ts
@@ -0,0 +1,10 @@
+const taskContainer = {
+  display: "flex",
+  alignItems: "center",
+  border: "0.1rem solid grey",
+  padding: "0.5rem 0.5rem",
+  borderRadius: "0.5rem",
+  marginBottom: "0.5rem",
+  justifyContent: "space-between",
+};
+export default taskContainer;
